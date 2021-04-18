31eb3654e17c9d2fd7511dd850553ece31a3602b
Rob Probin <rob.probin@gmail.com>
2021-04-18 15:01:17 +0100

    Changes to movement config

--- mazerunner/config.h
+++ mazerunner/config.h
@@ -43,9 +43,9 @@ const bool USER_MODE = false;
 //***************************************************************************//
 // We need to know about the drive mechanics.
 
-const float WHEEL_DIAMETER = 32.5; // Adjust on test
+const float WHEEL_DIAMETER = 34.23; // Adjust on test
 const float ENCODER_PULSES = 12.0;
-const float GEAR_RATIO = 19.5;
+const float GEAR_RATIO = 19.54;
 
 // Mouse radius is the distance between the contact patches of the drive wheels.
 // A good starting approximation is half the distance between the wheel centres.
